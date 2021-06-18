96b35b8f0c8dd026dcd96b49985bb921a5e84dd7
Mohammed Chakmakchi <mohammed.chakmakchi@lego.com>
2021-06-18 23:45:59 +0200

    test

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
+const path = require('path');
+
 
 try {
     // `who-to-greet` input defined in action metadata file
@@ -11,7 +13,13 @@ try {
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
-    fs.writeFileSync('text.txt', payload);
+    fs.mkdir(path.join(__dirname, 'output'), (err) => {
+        if (err) {
+            return console.error(err);
+        }
+        console.log('Directory created successfully!');
+    });
+    fs.writeFileSync('/output/text.txt', payload);
 
 } catch (error) {
     core.setFailed(error.message);
