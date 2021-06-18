755f382adb69360991026e3e08c88198d84f8ac0
Mohammed Chakmakchi <mohammed.chakmakchi@lego.com>
2021-06-18 23:48:42 +0200

    test

--- index.js
+++ index.js
@@ -3,24 +3,27 @@ const github = require('@actions/github');
 const fs = require('fs');
 const path = require('path');
 
+async function f() {
+    try {
+        // `who-to-greet` input defined in action metadata file
+        const nameToGreet = core.getInput('who-to-greet');
+        console.log(`Hello ${nameToGreet}!`);
+        const time = (new Date()).toTimeString();
+        core.setOutput("time", time);
+        // Get the JSON webhook payload for the event that triggered the workflow
+        const payload = JSON.stringify(github.context.payload, undefined, 2)
+        console.log(`The event payload: ${payload}`);
+        await fs.mkdir(path.join(__dirname, 'output'), (err) => {
+            if (err) {
+                return console.error(err);
+            }
+            console.log('Directory created successfully!');
+        });
+        fs.writeFileSync('/output/text.txt', payload);
 
-try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-    fs.mkdir(path.join(__dirname, 'output'), (err) => {
-        if (err) {
-            return console.error(err);
-        }
-        console.log('Directory created successfully!');
-    });
-    fs.writeFileSync('/output/text.txt', payload);
-
-} catch (error) {
-    core.setFailed(error.message);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+
+f();
